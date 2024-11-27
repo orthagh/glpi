2b2b09089017ccde5f22c3b79b891c984a09d078
Alexandre Delaunay <adelaunay@teclib.com>
2024-11-27 11:52:44 +0100

    another way

--- .github/actions/test_tests-e2e.sh
+++ .github/actions/test_tests-e2e.sh
@@ -7,8 +7,5 @@ export CYPRESS_BASE_URL="http://localhost:80"
 # Install Cypress
 node_modules/.bin/cypress install
 
-CHROME_PATH=${{ steps.setup-chrome.outputs.chrome-path }}
-echo "CHROME_PATH: $CHROME_PATH"
-
 # Run Cypress tests
 node_modules/.bin/cypress run --project tests --browser $CHROME_PATH
