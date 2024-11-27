97a3a61abb8ff9302bb5daf0c869b7f0d1a75f9b
Alexandre Delaunay <adelaunay@teclib.com>
2024-11-27 12:30:13 +0100

    compose command

--- .github/actions/test_tests-e2e.sh
+++ .github/actions/test_tests-e2e.sh
@@ -7,11 +7,5 @@ export CYPRESS_BASE_URL="http://localhost:80"
 # Install Cypress
 node_modules/.bin/cypress install
 
-# Install chrome
-sudo apt-get install -S -y wget
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -S -y ./google-chrome-stable_current_amd64.deb
-chrome --version
-
 # Run Cypress tests
 node_modules/.bin/cypress run --project tests --browser chrome
