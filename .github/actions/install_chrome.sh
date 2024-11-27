7b55c5b12b88f425f3afd60ad72cda56f8e23cb4
Alexandre Delaunay <adelaunay@teclib.com>
2024-11-27 14:33:26 +0100

    missing shebang

--- .github/actions/install_chrome.sh
+++ .github/actions/install_chrome.sh
@@ -1,4 +1,5 @@
-# Install chrome
+#!/bin/bash
+
 apt-get install -y wget
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -S -y ./google-chrome-stable_current_amd64.deb
