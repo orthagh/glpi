81d093f6a40a8f27eb4faeddbb7a910348427696
Alexandre Delaunay <adelaunay@teclib.com>
2024-11-27 14:36:32 +0100

    bad option

--- .github/actions/install_chrome.sh
+++ .github/actions/install_chrome.sh
@@ -2,5 +2,5 @@
 
 apt-get install -y wget
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -S -y ./google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
 chrome --version
